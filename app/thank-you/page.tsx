e0dd191f5207b3f154fd7c0e1e59f7c37b25395f
AasthaCodexorr <aastha.codexorr@gmail.com>
2026-08-14 15:58:00 +0530

    updated thank you page

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -1,14 +1,11 @@
-import { GetInTouch } from '@/components/common';
 import { Footer, Header } from '@/components/layout';
-import Image from 'next/image';
-import thanksImg from "@/assets/icons/thanksImg.jpg";
 import Link from 'next/link';
 
 export default async function ThankYou() {
     return (
         <>
             <Header />
-             <div className='lg:mt-20 pt-14 lg:pt-28 lg:pb-20 px-4 pb-10 lg:px-44 w-full flex flex-col justify-center items-center gap-2 bg-gradient-to-b from-prequalify-blue to-white'>
+             <div className='lg:mt-20 pt-14 lg:pt-28 lg:pb-20 px-4 pb-10 lg:px-44 mb-10 w-full flex flex-col justify-center items-center gap-2  '>
                 <h1 className='lg:text-[40px] text-[26px] text-center font-semibold'>Thank you for submitting your form!</h1>
                 <h3 className='lg:text-[24px] text-[24px] text-center font-medium'>We respond within 2 Business Hours.</h3>
                 <div className='mt-8'>
@@ -16,18 +13,7 @@ export default async function ThankYou() {
                     Go Back to Home Page
                   </Link>
                 </div>
-            </div>
-
-            <div className='w-full lg:px-48 lg:pb-20 px-4 pb-8 '>
-                <Image
-                    src={thanksImg}
-                    alt="Blue Honda Civic Sedan showcasing GrCars Certified quality"
-                    className="w-full h-auto object-contain"
-                    width={1000}
-                    height={500}
-                />
-            </div>
-            
+            </div>           
             <Footer />
         </>
     );
