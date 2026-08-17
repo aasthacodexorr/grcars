4d708c8f463c1376b6d773d471552920b263349d
AasthaCodexorr <aastha.codexorr@gmail.com>
2026-08-17 15:58:08 +0530

    updated logo in footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -8,6 +8,8 @@ import { usePathname } from "next/navigation";
 import zlogo from "@/assets/brand/zlogo.png";
 import { useAppConfig } from "@/app/providers";
 import { fallbackValue, defaultAppConfig } from "@/lib/appConfig";
+import logo from "@/assets/pages/grcarslogo.png";
+
 
 const Footer = () => {
   const appConfig = useAppConfig();
@@ -128,12 +130,8 @@ const Footer = () => {
                   />
                 </div>
               ) : (
-                <div className="w-12 h-12 bg-[#0d82df] rounded-full flex items-center justify-center shadow-md">
-                  <svg className="w-6 h-6 text-white fill-current" viewBox="0 0 24 24">
-                    <path d="M18.92 6.01C18.72 5.42 18.16 5 17.5 5h-11c-.66 0-1.21.42-1.42 1.01L3 12v8c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h12v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-8l-2.08-5.99zM6.85 7h10.29l1.04 3H5.81l1.04-3zM19 17H5v-4h14v4z" />
-                    <circle cx="7.5" cy="15" r="1.5" />
-                    <circle cx="16.5" cy="15" r="1.5" />
-                  </svg>
+                <div className=" rounded-full flex items-center justify-center shadow-md">
+                   <img src={logo?.src}/>
                 </div>
               )}
             </Link>
