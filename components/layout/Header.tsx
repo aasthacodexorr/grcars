3530208452f30e262a42e5458137d03eb3c7c887
AasthaCodexorr <aastha.codexorr@gmail.com>
2026-08-25 19:31:23 +0530

    fixed the ui in mobile view of contact us page

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -168,7 +168,7 @@ const Header = () => {
 
         {/* Green action bar: call, directions, menu toggle */}
         <div className={`text-white flex items-center justify-between py-2 relative border-t border-b border-neutral-mediumGray`}>
-          <div className="flex items-center gap-5 px-3 top-0">
+          <div className="flex items-center gap-5 px-8 top-0">
 
             {/* Call */}
             <a
