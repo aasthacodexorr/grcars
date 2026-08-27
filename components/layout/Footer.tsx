c0e5e83637db0a40e3f137edd46de5ff4eb22f3a
AasthaCodexorr <aastha.codexorr@gmail.com>
2026-08-27 12:27:44 +0530

    Decrease the space from the bottom. Match the bottom space with carvana.

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -118,7 +118,7 @@ const Footer = () => {
 
   return (
     <footer className="w-full font-sans">
-      <div className="max-w-[1550px] mx-auto bg-[#101827] text-white p-8 sm:p-12 lg:px-28 py-16">
+      <div className="max-w-[1550px] mx-auto bg-[#101827] text-white px-8 pt-8 pb-4 sm:px-12 lg:px-28 ">
 
         {/* Main Grid: Logo + Nav Columns */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-8 lg:gap-12 mb-1">
