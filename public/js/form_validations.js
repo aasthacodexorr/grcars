a3b0137402a33d436200ed6ee48f3054222c1716
AasthaCodexorr <aastha.codexorr@gmail.com>
2026-09-08 18:44:46 +0530

    updated script for forms

--- public/js/form_validations.js
+++ public/js/form_validations.js
@@ -1,10 +1,11 @@
 const ALLOWED_IFRAME_ORIGIN = "https://gediroute.zopsoftware.com";
 
-$( document ).ready(function() {
+$(document).ready(function () {
 
     function wait(ms) {
         var start = new Date().getTime();
         var end = start;
+
         while (end < start + ms) {
             end = new Date().getTime();
         }
@@ -19,65 +20,78 @@ $( document ).ready(function() {
             alert.showLoading();
         },
     });
-    
-    window.addEventListener("message", function(event) {
 
-		if (typeof global_properties != "undefined") {
-            if (event.origin !== ALLOWED_IFRAME_ORIGIN) {
-                return;
-            }
+    window.addEventListener("message", function (event) {
+
+        // Allow messages only from the trusted iframe origin
+        if (event.origin !== ALLOWED_IFRAME_ORIGIN) {
+            return;
         }
 
         switch (event.data) {
-            case 'alertFire':
+
+            case "alertFire":
                 alert.fire();
-                console.log('alertFire');
+                console.log("alertFire");
                 break;
-            case 'alertSuccessUpdate':
-                console.log('alertSuccessUpdate');
-                 alert.update({
-                    title: 'Thank You! \n Your message has been sent.',
-                    icon: "success",
-                }); 
-                break;
-            case 'redirectToThankYouPage':
-                console.log('redirectToThankYouPage');
-                window.location.href = window.location.origin + '/thank-you';
-                break;
-				
-			case 'redirectToInventoryListing':
-                console.log('redirectToInventoryListing');
-                window.location.href = window.location.origin + '/inventory';
-                break;
-				
-            case 'alertErrorUpdate':
-                console.log('alertErrorUpdate');
+
+            case "alertSuccessUpdate":
+                console.log("alertSuccessUpdate");
+
                 alert.update({
-                    title: 'There were errors in submitting the form.',
+                    title: "Thank You! \n Your message has been sent.",
+                    icon: "success",
+                });
+                break;
+
+            case "redirectToThankYouPage":
+                console.log("redirectToThankYouPage");
+                window.location.href =
+                    window.location.origin + "/thank-you";
+                break;
+
+            case "redirectToInventoryListing":
+                console.log("redirectToInventoryListing");
+                window.location.href =
+                    window.location.origin + "/inventory";
+                break;
+
+            case "alertErrorUpdate":
+                console.log("alertErrorUpdate");
+
+                alert.update({
+                    title: "There were errors in submitting the form.",
                     icon: "error",
                 });
                 break;
-            case 'alertClose':
-                console.log('alertClose');
+
+            case "alertClose":
+                console.log("alertClose");
                 wait(1500);
                 alert.close();
                 break;
-            case 'singleCreditApplicationHeight':
-                console.log('singleCreditApplicationHeight')
+
+            case "singleCreditApplicationHeight":
+                console.log("singleCreditApplicationHeight");
                 break;
+
             default:
                 break;
         }
 
-        if(event.data.hasOwnProperty("type")){
-			console.log(event);
-            if(event.data.type == "css"){
-                $(`#${event.data.element_id}`).css("min-height", parseInt(event.data.value) + 80);
+        if (
+            event.data &&
+            typeof event.data === "object" &&
+            event.data.hasOwnProperty("type")
+        ) {
+            console.log(event);
+
+            if (event.data.type === "css") {
+                $(`#${event.data.element_id}`).css(
+                    "min-height",
+                    parseInt(event.data.value) + 80
+                );
             }
         }
-
-         
     });
-
-
 });
