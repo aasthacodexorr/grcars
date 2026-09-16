6ad03ff8f4d177e6aae587822c5272438e1c4b0a
AasthaCodexorr <aastha.codexorr@gmail.com>
2026-09-16 16:18:30 +0530

    rishabh credit iframe

--- constants/index.ts
+++ constants/index.ts
@@ -180,6 +180,7 @@ export const getConstants = (appConfig: AppConfig) => {
         creditMayur:`${appConfig.site.saas_api}/api/templates/render/14?assigneeid=17`,
         creditPranav:`${appConfig.site.saas_api}/api/templates/render/14?assigneeid=2`,
         creditDevin:`${appConfig.site.saas_api}/api/templates/render/14?assigneeid=4`,
+        creditRishabh:`${appConfig.site.saas_api}/api/templates/render/14?assigneeid=7`,
 
 
 
